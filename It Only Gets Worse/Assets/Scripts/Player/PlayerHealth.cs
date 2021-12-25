170a7019d3dd3e486c55627d00e412fcca4081b2
Jb645 <50148003+Jb645@users.noreply.github.com>
2021-12-25 18:10:01 -0500

    Added Health Bar for Player, added Scriptable Objects for Enemyes and players

---
+++ It Only Gets Worse/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth;
+
+    private int currentHealth;
+
+    private HealthBar healthBar;
+
+    //set up the scriptable object
+    [SerializeField] private Entity playerEntity;
+
+    private void Awake()
+    {
+        maxHealth = playerEntity.maxHealth;
+        healthBar = GetComponent<HealthBar>();
+    }
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    private void Update()
+    {
+        //testing
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            TakeDamage(20);
+        }
+    }
+
+    private void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.setHealth(currentHealth);
+    }
+
+    private void Heal(int healAmount)
+    {
+        currentHealth += healAmount;
+        healthBar.setHealth(currentHealth);
+    }
+}
---
+++ It Only Gets Worse/Assets/Scripts/PointSystem/Scritables/Entity.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Entity", menuName = "Entity")]
+public class Entity : ScriptableObject
+{
+    public new string name;
+    public string type;
+
+    public int damage;
+    public int maxHealth;
+    public int speed_walking;
+    public int speed_running;
+}
