c782c186271d73f5f45a63b8bd76f0e4e4f2c66b
Jb645 <50148003+Jb645@users.noreply.github.com>
2021-12-25 18:28:52 -0500

    Enemy Health script created, and updated Scriptables.

---
+++ It Only Gets Worse/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] private int maxHealth;
+    private float deathAnimationTime;
+    private int currentHealth;
+    public Entity enemyEntity;
+
+    private void Start()
+    {
+        maxHealth = enemyEntity.maxHealth;
+        currentHealth = maxHealth;
+
+        deathAnimationTime = enemyEntity.DAT;
+    }
+
+    private void Update()
+    {
+        //testing
+        Testing();
+    }
+
+    private void Testing()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            takeDamage(10);
+        }
+    }
+
+    private void takeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        //code for death animation
+
+        Destroy(this, deathAnimationTime);
+    }
+}
--- It Only Gets Worse/Assets/Scripts/PointSystem/Scritables/Entity.cs
+++ It Only Gets Worse/Assets/Scripts/PointSystem/Scritables/Entity.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Entity", menuName = "Entity")]
 public class Entity : ScriptableObject
 {
+    [Header("-Identity-")]
     public new string name;
+
     public string type;
 
+    [Header("-Stats-")]
     public int damage;
+
     public int maxHealth;
     public int speed_walking;
     public int speed_running;
+
+    [Header("-Animation-")]
+    [Tooltip("Death Animation Time, how long does the death animation take?")]
+    public float DAT;
 }
