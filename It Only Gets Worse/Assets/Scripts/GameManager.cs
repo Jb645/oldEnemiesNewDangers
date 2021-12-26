1421e8b17d95c12070c987cf107886c61cc9b0c8
Saharsh1223 <88390577+Saharsh1223@users.noreply.github.com>
2021-12-26 08:29:36 +0530

    Added AI Pathfinding

---
+++ It Only Gets Worse/Assets/Scripts/GameManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Target")]
+    [SerializeField] GameObject targetObject;
+
+    [Header("Instantiate Enemy")]
+    [SerializeField] GameObject enemyPrefab;
+    [SerializeField] Transform enemyParent;
+
+    [Header("Instantiate Boss")]
+    [SerializeField] GameObject bossPrefab;
+    [SerializeField] Transform bossParent;
+
+    [Header("Enemy Settings")]
+    [SerializeField] int enemiesToSpawn;
+
+    [Header("Boss Settings")]
+    [SerializeField] int bossesToSpawn;
+
+    private void Start()
+    {
+        SpawnEnemies();
+        SpawnBosses();
+    }
+
+    void SpawnEnemies()
+    {
+        //spawn enemies
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            enemyPrefab.transform.position = new Vector3(Random.Range(0f, 100f), 0f, Random.Range(0f, 100f));
+            GameObject newEnemy = Instantiate(enemyPrefab, enemyParent);
+
+            EnemyController enemyController = newEnemy.GetComponent<EnemyController>();
+            enemyController.playerToChase = targetObject.transform;
+        }
+    }
+
+    void SpawnBosses()
+    {
+        //spawn boss
+        for (int i = 0; i < bossesToSpawn; i++)
+        {
+            bossPrefab.transform.position = new Vector3(Random.Range(0f, 100f), 0f, Random.Range(0f, 100f));
+            GameObject newBoss = Instantiate(bossPrefab, bossParent);
+
+            BossController bossController = newBoss.GetComponent<BossController>();
+            bossController.playerToChase = targetObject.transform;
+        }
+    }
+}
