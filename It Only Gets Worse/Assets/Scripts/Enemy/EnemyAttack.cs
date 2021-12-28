3b43a2665cca59a958cd118f4135c46a8b44f450
Jb645 <50148003+Jb645@users.noreply.github.com>
2021-12-27 21:11:57 -0500

    player health bar works, point system now works too

---
+++ It Only Gets Worse/Assets/Scripts/Enemy/EnemyAttack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    private Rigidbody enemybody;
+
+    [SerializeField]
+    private float hitRate = 15f;
+
+    private float nextTimeToAttack;
+
+    [SerializeField]
+    private Entity enemyType;
+
+    private void Start()
+    {
+        enemybody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.transform.gameObject.CompareTag("Player")) return;
+
+        if (Time.time > nextTimeToAttack)
+        {
+            nextTimeToAttack = Time.time + 1f / hitRate;
+            Debug.Log("you got hit");
+            collision.transform.gameObject.GetComponent<PlayerHealth>().TakeDamage(enemyType.damage);
+        }
+    }
+}
---
+++ It Only Gets Worse/Assets/Scripts/Enemy/EnemyCounter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EnemyCounter : MonoBehaviour
+{
+    private static int EnemiesNum;
+
+    private Entity basicEnemy, bossEnemy;
+    private PointTracker point;
+
+    private void Awake()
+    {
+        basicEnemy = ScriptableTags.basicEnemyEntity;
+        bossEnemy = ScriptableTags.bossEnemyEntity;
+        EnemiesNum = basicEnemy.num + bossEnemy.num;
+    }
+
+    public static void OnEnemyDeath()
+    {
+        EnemiesNum -= 1;
+
+        if (EnemiesNum <= 0)
+        {
+            SceneManager.LoadScene(SceneTags.Intro);
+        }
+    }
+}
